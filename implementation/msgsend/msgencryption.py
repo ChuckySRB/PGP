675a627335b8c78a9ead2a404fa65ec6601bad8c
ChuckySRB <chucky.srb@gmail.com>
2023-06-17 22:17:38 +0200

    KeyManger Get Public Key method

--- implementation/msgsend/msgencryption.py
+++ implementation/msgsend/msgencryption.py
@@ -1,5 +1,8 @@
+from implementation.keymanagement.keymanager import *
+
+
 class MessageEncryption:
 
     @staticmethod
-    def encrypt():
-        pass
+    def encrypt(email, private_key, public_key, authentification, privacy_algorthm, ZIP, RADIX):
+        keyManager = KeyManager()
